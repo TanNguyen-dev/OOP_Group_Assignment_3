1d58a0329987fccfb3439675f1876129fdae8194
Tan Nguyen <151684402+TanNguyen-dev@users.noreply.github.com>
2024-03-18 10:01:33 -0600

    Add files via upload

---
+++ SinglyLinkedListTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using SinglyLinkedListBase;
+
+namespace SinglyLinkedListTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
