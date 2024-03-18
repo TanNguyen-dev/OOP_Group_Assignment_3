b2fff751b3d0e43572729357873aa6d5f0bfade3
Tan Nguyen <151684402+TanNguyen-dev@users.noreply.github.com>
2024-03-18 10:00:40 -0600

    Add files via upload

---
+++ Assignment_3_skeleton/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_skeleton
+{
+    internal class Node
+    {
+        // Data or value stored in the node.
+        public object Data { get; set; }
+
+        // Reference to the next node in the linked list.
+        public Node Next { get; set; }
+
+        // Node class constructor
+        public Node(object data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
---
+++ Assignment_3_skeleton/SinglyLinkedListBase.cs
@@ -0,0 +1,80 @@
+﻿namespace Assignment_3_skeleton
+{
+    public class SinglyLinkedListBase : ILinkedListADT
+    {
+
+        //Define reference to the firts node
+        private Node head;
+        private int listSize;
+
+        //SLL constructor
+        public SinglyLinkedListBase()
+        {
+            head = null;
+        }
+
+        public bool IsEmpty()
+        {
+            if (head is null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            listSize = 0;
+        }
+
+        public void Prepend(object data)
+        {
+            //Create new node
+            Node newNode = new Node(data);
+            //Set the new node to the head
+            newNode.Next = head;
+            //Update head to the next node
+            head = newNode;
+
+            //Update listSize
+            listSize++;
+        }
+
+        public void Append(object data)
+        {
+            Node newNode = new Node(data);
+            if (head is null)
+            {
+                //Update head to the newNode;
+                head = newNode;
+                listSize++;
+                return;
+            }
+
+            //Interate through the SLL
+            Node currentNode = head;
+            while (currentNode.Next != null)
+            {
+                // Go to the next node 
+                currentNode = currentNode.Next;
+            }
+
+            // Update the last node to point to the new node
+            currentNode.Next = newNode;
+
+            //Update listSize
+            listSize++;
+        }
+
+        public int Size()
+        {
+            //Checking if SLL is empty
+            if (head is null)
+            {
+                return 0;
+            }
+            return listSize;
+        }
+    }
+}
