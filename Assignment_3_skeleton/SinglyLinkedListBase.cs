a569570459faf2635314246931c1c6bc2140fc39
Tan Nguyen <151684402+TanNguyen-dev@users.noreply.github.com>
2024-03-29 23:06:47 -0600

    Update SinglyLinkedListBase.cs

--- Assignment_3_skeleton/SinglyLinkedListBase.cs
+++ Assignment_3_skeleton/SinglyLinkedListBase.cs
@@ -5,170 +5,143 @@ namespace Assignment_3_skeleton
     public class SinglyLinkedListBase : ILinkedListADT
     {
 
-        /        //Define reference to the firts node
-        private Node head;
-        private int listSize;
+    private Node head;
+    private int listSize;
 
-        //SLL constructor
-        public SinglyLinkedListBase()
-        {
-            head = null;
-        }
+    public SinglyLinkedListBase()
+    {
+        head = null;
+        listSize = 0;
+    }
 
-        public bool IsEmpty()
-        {
-            if (head is null)
-            {
-                return true;
-            }
-            return false;
-        }
+    public bool IsEmpty()
+    {
+        return head == null;
+    }
 
-        public void Clear()
-        {
-            head = null;
-            listSize = 0;
-        }
+    public void Clear()
+    {
+        head = null;
+        listSize = 0;
+    }
 
-        public void Prepend(object data)
+    public void Prepend(object data)
+    {
+        Node newNode = new Node(data);
+        newNode.Next = head;
+        head = newNode;
+        listSize++;
+    }
+
+    public void Append(object data)
+    {
+        Node newNode = new Node(data);
+        if (head == null)
         {
-            //Create new node
-            Node newNode = new Node(data);
-            //Set the new node to the head
-            newNode.Next = head;
-            //Update head to the next node
             head = newNode;
-
-            //Update listSize
-            listSize++;
         }
-
-        public void Append(object data)
+        else
         {
-            Node newNode = new Node(data);
-            if (head is null)
-            {
-                //Update head to the newNode;
-                head = newNode;
-                listSize++;
-                return;
-            }
-
-            //Interate through the SLL
             Node currentNode = head;
             while (currentNode.Next != null)
             {
-                // Go to the next node 
                 currentNode = currentNode.Next;
             }
-
-            // Update the last node to point to the new node
             currentNode.Next = newNode;
+        }
+        listSize++;
+    }
 
-            //Update listSize
+    public int Size()
+    {
+        return listSize;
+    }
+
+    public void Insert(object data, int index)
+    {
+        if (index < 0 || index > listSize)
+        {
+            throw new IndexOutOfRangeException("Index is out of bounds.");
+        }
+        if (index == 0)
+        {
+            Prepend(data);
+        }
+        else if (index == listSize)
+        {
+            Append(data);
+        }
+        else
+        {
+            Node newNode = new Node(data);
+            Node prevNode = GetNodeAtIndex(index - 1);
+            newNode.Next = prevNode.Next;
+            prevNode.Next = newNode;
             listSize++;
         }
+    }
 
-        public int Size()
+    public void Replace(object data, int index)
+    {
+        Node node = GetNodeAtIndex(index);
+        node.Data = data;
+    }
+
+    public void Delete(int index)
+    {
+        if (index < 0 || index >= listSize)
         {
-            //Checking if SLL is empty
-            if (head is null)
-            {
-                return 0;
-            }
-            return listSize;
+            throw new IndexOutOfRangeException("Index is out of bounds.");
         }
-
-        public void Insert(object data, int index)
+        if (index == 0)
         {
-            if (index == 0)
-            {
-                Prepend(data);
-            }
-            else if (index == listSize)
-            {
-                Append(data);
-            }
-            else
-            {
-                Node newNode = new Node(data);
-                Node prevNode = GetNodeAtIndex(index - 1);
-                newNode.Next = prevNode.Next;
-                prevNode.Next = newNode;
-                listSize++;
-            }
+            head = head.Next;
         }
-
-        public void Replace(object data, int index)
+        else
         {
-            Node node = GetNodeAtIndex(index);
-            node.Data = data;
+            Node prevNode = GetNodeAtIndex(index - 1);
+            prevNode.Next = prevNode.Next.Next;
         }
+        listSize--;
+    }
 
-        public void Delete(int index)
+    private Node GetNodeAtIndex(int index)
+    {
+        if (index < 0 || index >= listSize)
         {
-            if (index == 0)
-            {
-                head = head.Next;
-            }
-            else
-            {
-                Node prevNode = GetNodeAtIndex(index - 1);
-                prevNode.Next = prevNode.Next.Next;
-            }
-            listSize--;
+            throw new IndexOutOfRangeException("Index is out of bounds.");
         }
-
-
-        private Node GetNodeAtIndex(int index)
+        Node currentNode = head;
+        for (int i = 0; i < index; i++)
         {
-            if (index < 0 || index >= listSize)
-            {
-                throw new IndexOutOfRangeException("Index is out of bounds.");
-            }
-            Node currentNode = head;
-            for (int i = 0; i < index; i++)
-            {
-                currentNode = currentNode.Next;
-            }
-            return currentNode;
+            currentNode = currentNode.Next;
         }
+        return currentNode;
+    }
 
-        public Object Retrieve(int index)
+    public object Retrieve(int index)
+    {
+        return GetNodeAtIndex(index).Data;
+    }
+
+    public int IndexOf(object data)
+    {
+        Node currentNode = head;
+        int index = 0;
+        while (currentNode != null)
         {
-            if (index < 0 || index >= listSize)
+            if ((currentNode.Data == null && data == null) || (currentNode.Data != null && currentNode.Data.Equals(data)))
             {
-                throw new IndexOutOfRangeException("Index is out of bounds.");
+                return index;
             }
-
-            Node currentNode = head;
-            for (int i = 0; i < index; i++)
-            {
-                currentNode = currentNode.Next;
-            }
-            return currentNode.Data != null ? currentNode.Data : null;
+            currentNode = currentNode.Next;
+            index++;
         }
+        return -1;
+    }
 
-        public int IndexOf(Object data)
-        {
-            int index = 0;
-            Node currentNode = head;
-
-            while (currentNode != null)
-            {
-                if ((currentNode.Data == null && data == null) || (currentNode.Data != null && currentNode.Data.Equals(data)))
-                {
-                    return index;
-                }
-                currentNode = currentNode.Next;
-                index++;
-            }
-            return -1;
-        }
-
-        public bool Contains(Object data)
-        {
-            return IndexOf(data) != -1;
-        }
+    public bool Contains(object data)
+    {
+        return IndexOf(data) != -1;
     }
 }
